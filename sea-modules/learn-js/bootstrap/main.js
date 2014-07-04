916b9c19d87246e4ee7dcc277128b31d2e637dcf
bsky1wcj <bsky1wcj@163.com>
2014-07-04 11:09:49 +0800

    提交sea-modules目录

---
+++ sea-modules/learn-js/bootstrap/main.js
@@ -0,0 +1,2 @@
+/*! learn-js-0.0.1 2014-07-04 10:42:35 */
+define("learn-js/bootstrap/main",["jquery/jquery/2.1.0/jquery"],function(a){var b=a("jquery/jquery/2.1.0/jquery");b("body").html("Hello World!")});
